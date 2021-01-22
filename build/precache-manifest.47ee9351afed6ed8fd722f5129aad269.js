1779a6b94e0470e02268401e79e37c2293e7ee43
ironcheftoshi <ironcheftoshi@gmail.com>
2021-01-21 18:59:37 -0600

    update3

---
+++ build/precache-manifest.47ee9351afed6ed8fd722f5129aad269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3fd91090a320da4b1a2",
+    "url": "/static/css/main.c4160aa9.chunk.css"
+  },
+  {
+    "revision": "a3fd91090a320da4b1a2",
+    "url": "/static/js/main.5f98feae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e94d5c3bf83dc82ccfac",
+    "url": "/static/css/2.2c8c524e.chunk.css"
+  },
+  {
+    "revision": "e94d5c3bf83dc82ccfac",
+    "url": "/static/js/2.da38742b.chunk.js"
+  },
+  {
+    "revision": "6803700ea153d94de54aa7fb55b08949",
+    "url": "/index.html"
+  }
+];
